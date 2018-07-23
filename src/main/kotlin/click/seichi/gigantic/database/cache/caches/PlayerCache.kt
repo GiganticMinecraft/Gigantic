06f80a80ab3cf0eb6076bedfd10153a2a20fc15c
tar0ss <syas1215@icloud.com>
2018-07-24 03:49:48 +0900

    Add all player data

--- src/main/kotlin/click/seichi/gigantic/database/cache/caches/PlayerCache.kt
+++ src/main/kotlin/click/seichi/gigantic/database/cache/caches/PlayerCache.kt
@@ -9,19 +9,63 @@ import java.util.*
  * @author tar0ss
  */
 class PlayerCache(val uniqueId: UUID, val playerName: String) : DatabaseCache<PlayerCache>(
-        PlayerCacheKeys.LOCALE
+        PlayerCacheKeys.LOCALE,
+        PlayerCacheKeys.MANA,
+        *PlayerCacheKeys.MINEBLOCK_MAP.values.toTypedArray(),
+        *PlayerCacheKeys.MEMORY_MAP.values.toTypedArray(),
+        *PlayerCacheKeys.APTITUDE_MAP.values.toTypedArray(),
+        *PlayerCacheKeys.BOSS_MAP.values.toTypedArray(),
+        *PlayerCacheKeys.RELIC_MAP.values.toTypedArray()
 ) {
     override fun read() {
-        val userEntityData = UserEntityData(uniqueId)
-        PlayerCacheKeys.LOCALE.let {
-            put(it, it.read(userEntityData.user))
+        UserEntityData(uniqueId).run {
+            PlayerCacheKeys.LOCALE.let {
+                put(it, it.read(user))
+            }
+            PlayerCacheKeys.MANA.let {
+                put(it, it.read(user))
+            }
+            PlayerCacheKeys.MINEBLOCK_MAP.forEach { reason, key ->
+                put(key, key.read(userMineBlockMap[reason] ?: return@forEach))
+            }
+            PlayerCacheKeys.MEMORY_MAP.forEach { will, key ->
+                put(key, key.read(userWillMap[will] ?: return@forEach))
+            }
+            PlayerCacheKeys.APTITUDE_MAP.forEach { will, key ->
+                put(key, key.read(userWillMap[will] ?: return@forEach))
+            }
+            PlayerCacheKeys.BOSS_MAP.forEach { boss, key ->
+                put(key, key.read(userBossMap[boss] ?: return@forEach))
+            }
+            PlayerCacheKeys.RELIC_MAP.forEach { relic, key ->
+                put(key, key.read(userRelicMap[relic] ?: return@forEach))
+            }
         }
     }
 
     override fun write() {
-        val userEntityData = UserEntityData(uniqueId)
-        PlayerCacheKeys.LOCALE.let {
-            it.store(userEntityData.user, get(it) ?: return@let)
+        UserEntityData(uniqueId).run {
+            PlayerCacheKeys.LOCALE.let {
+                it.store(user, get(it) ?: return@let)
+            }
+            PlayerCacheKeys.MANA.let {
+                it.store(user, get(it) ?: return@let)
+            }
+            PlayerCacheKeys.MINEBLOCK_MAP.forEach { reason, key ->
+                key.store(userMineBlockMap[reason] ?: return@forEach, get(key) ?: return@forEach)
+            }
+            PlayerCacheKeys.MEMORY_MAP.forEach { will, key ->
+                key.store(userWillMap[will] ?: return@forEach, get(key) ?: return@forEach)
+            }
+            PlayerCacheKeys.APTITUDE_MAP.forEach { will, key ->
+                key.store(userWillMap[will] ?: return@forEach, get(key) ?: return@forEach)
+            }
+            PlayerCacheKeys.BOSS_MAP.forEach { boss, key ->
+                key.store(userBossMap[boss] ?: return@forEach, get(key) ?: return@forEach)
+            }
+            PlayerCacheKeys.RELIC_MAP.forEach { relic, key ->
+                key.store(userRelicMap[relic] ?: return@forEach, get(key) ?: return@forEach)
+            }
         }
     }
 }
--- src/main/kotlin/click/seichi/gigantic/database/cache/keys/PlayerCacheKeys.kt
+++ src/main/kotlin/click/seichi/gigantic/database/cache/keys/PlayerCacheKeys.kt
@@ -1,8 +1,12 @@
 package click.seichi.gigantic.database.cache.keys
 
+import click.seichi.gigantic.boss.Boss
 import click.seichi.gigantic.database.cache.CacheKey
 import click.seichi.gigantic.database.cache.caches.PlayerCache
-import click.seichi.gigantic.database.dao.User
+import click.seichi.gigantic.database.dao.*
+import click.seichi.gigantic.player.MineBlockReason
+import click.seichi.gigantic.relic.Relic
+import click.seichi.gigantic.will.Will
 import org.jetbrains.exposed.dao.Entity
 import java.util.*
 
@@ -11,7 +15,7 @@ import java.util.*
  */
 object PlayerCacheKeys {
 
-    val LOCALE = object : CacheKey<PlayerCache, Locale> {
+    val LOCALE = object : PlayerCacheKey<Locale> {
         override val default
             get() = Locale.JAPANESE
 
@@ -26,4 +30,115 @@ object PlayerCacheKeys {
         }
     }
 
+    val MANA = object : PlayerCacheKey<Long> {
+        override val default: Long
+            get() = 0L
+
+        override fun read(entity: Entity<*>): Long? {
+            val user = entity as User
+            return user.mana
+        }
+
+        override fun store(entity: Entity<*>, value: Long) {
+            val user = entity as User
+            user.mana = value
+        }
+    }
+
+    val MINEBLOCK_MAP = MineBlockReason.values()
+            .map { it to MineBlockCacheKey(it) }
+            .toMap()
+
+    val MEMORY_MAP = Will.values()
+            .map { it to MemoryCacheKey(it) }
+            .toMap()
+
+    val APTITUDE_MAP = Will.values()
+            .map { it to AptitudeCacheKey(it) }
+            .toMap()
+
+    val BOSS_MAP = Boss.values()
+            .map { it to BossCacheKey(it) }
+            .toMap()
+
+    val RELIC_MAP = Relic.values()
+            .map { it to RelicCacheKey(it) }
+            .toMap()
+
+    class MineBlockCacheKey(mineBlockReason: MineBlockReason) : PlayerCacheKey<Long> {
+        override val default: Long
+            get() = 0L
+
+        override fun read(entity: Entity<*>): Long? {
+            val userMineBlock = entity as UserMineBlock
+            return userMineBlock.mineBlock
+        }
+
+        override fun store(entity: Entity<*>, value: Long) {
+            val userMineBlock = entity as UserMineBlock
+            userMineBlock.mineBlock = value
+        }
+    }
+
+    class MemoryCacheKey(will: Will) : PlayerCacheKey<Long> {
+        override val default: Long
+            get() = 0L
+
+        override fun read(entity: Entity<*>): Long? {
+            val userWill = entity as UserWill
+            return userWill.memory
+        }
+
+        override fun store(entity: Entity<*>, value: Long) {
+            val userWill = entity as UserWill
+            userWill.memory = value
+        }
+    }
+
+    class AptitudeCacheKey(will: Will) : PlayerCacheKey<Boolean> {
+        override val default: Boolean
+            get() = false
+
+        override fun read(entity: Entity<*>): Boolean? {
+            val userWill = entity as UserWill
+            return userWill.hasAptitude
+        }
+
+        override fun store(entity: Entity<*>, value: Boolean) {
+            val userWill = entity as UserWill
+            userWill.hasAptitude = value
+        }
+    }
+
+    class BossCacheKey(boss: Boss) : PlayerCacheKey<Long> {
+        override val default: Long
+            get() = 0L
+
+        override fun read(entity: Entity<*>): Long? {
+            val userBoss = entity as UserBoss
+            return userBoss.defeat
+        }
+
+        override fun store(entity: Entity<*>, value: Long) {
+            val userBoss = entity as UserBoss
+            userBoss.defeat = value
+        }
+    }
+
+    class RelicCacheKey(relic: Relic) : PlayerCacheKey<Long> {
+        override val default: Long
+            get() = 0L
+
+        override fun read(entity: Entity<*>): Long? {
+            val userRelic = entity as UserRelic
+            return userRelic.amount
+        }
+
+        override fun store(entity: Entity<*>, value: Long) {
+            val userRelic = entity as UserRelic
+            userRelic.amount = value
+        }
+    }
+
+    interface PlayerCacheKey<V> : CacheKey<PlayerCache, V>
 }
