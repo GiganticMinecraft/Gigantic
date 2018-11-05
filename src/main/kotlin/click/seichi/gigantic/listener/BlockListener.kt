fa4bc0be8dd64bb2c6b82a3304483ed8a4d3ff21
tar0ss <syas1215@icloud.com>
2018-11-06 02:10:24 +0900

    Cancell spread grass

--- src/main/kotlin/click/seichi/gigantic/listener/BlockListener.kt
+++ src/main/kotlin/click/seichi/gigantic/listener/BlockListener.kt
@@ -1,9 +1,11 @@
 package click.seichi.gigantic.listener
 
+import org.bukkit.Material
 import org.bukkit.entity.FallingBlock
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockFromToEvent
+import org.bukkit.event.block.BlockSpreadEvent
 import org.bukkit.event.entity.EntityChangeBlockEvent
 
 /**
@@ -27,6 +29,15 @@ class BlockListener : Listener {
         if (block.type == material) {
             event.isCancelled = true
         }
+    }
+
+    // 芝生の増殖を抑止
+    @EventHandler
+    fun onBlockSpread(event: BlockSpreadEvent) {
+        if (event.isCancelled) return
+        if (event.newState.type != Material.GRASS_BLOCK) return
+        event.isCancelled = true
+
     }
 
 }
