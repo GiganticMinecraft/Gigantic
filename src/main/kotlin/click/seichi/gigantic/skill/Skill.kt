1fbb304edbeb362fb0a5da2a53df4dd44afe0ec7
tar0ss <syas1215@icloud.com>
2018-11-08 12:30:33 +0900

    Add skill and spell

---
+++ src/main/kotlin/click/seichi/gigantic/skill/Skill.kt
@@ -0,0 +1,12 @@
+package click.seichi.gigantic.skill
+
+/**
+ * @author tar0ss
+ */
+enum class Skill(
+        val id: Int
+) {
+    HEAL(1),
+    MINE_BURST(2)
+    ;
+}
---
+++ src/main/kotlin/click/seichi/gigantic/spell/Spell.kt
@@ -0,0 +1,15 @@
+package click.seichi.gigantic.spell
+
+/**
+ * @author tar0ss
+ */
+enum class Spell(
+        val id: Int
+) {
+    STELLA_CLAIR(0),
+    AQUA_LINEA(100),
+    IGNIS_VOLCANO(200),
+    AER_SLASH(300),
+    TERRA_DRAIN(400),
+    GRAND_NATURA(500),
+}
