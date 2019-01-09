f6f7c06fbef7369936b57a932db2d873b8a88311
unicroak <unicroak@gmail.com>
2019-01-09 23:47:17 +0900

    add forgotten velocity :P

--- src/main/kotlin/click/seichi/gigantic/popup/PopUp.kt
+++ src/main/kotlin/click/seichi/gigantic/popup/PopUp.kt
@@ -3,23 +3,27 @@ package click.seichi.gigantic.popup
 import click.seichi.gigantic.Gigantic
 import click.seichi.gigantic.extension.noised
 import click.seichi.gigantic.util.NoiseData
+import click.seichi.gigantic.util.Random
 import org.bukkit.Location
 import org.bukkit.entity.ArmorStand
 import org.bukkit.entity.Entity
+import org.bukkit.util.Vector
 
 /**
  * @author tar0ss
  * @author unicroak
  */
 sealed class PopUp(private val text: String?,
-                   private val popping: (ArmorStand) -> Unit) {
+                   private val customizeOnPop: ArmorStand.() -> Unit = {}) {
+    // HACK: [customizeOnPop] should not exist
 
     abstract val lifeTime: Long
 
-    open fun pop(location: Location, noise: NoiseData = NoiseData()): ArmorStand {
+    fun pop(location: Location, noise: NoiseData = NoiseData()): ArmorStand {
         return location.world.spawn(location.noised(noise), ArmorStand::class.java) {
-            it.modelAsPop(text, popping)
+            it.buildAsPop(text)
             Gigantic.PLUGIN.apply { server.scheduler.runTaskLater(this@apply, { _ -> if (it.isValid) it.remove() }, lifeTime) }
+            customizeOnPop(it)
         }
     }
 
@@ -31,8 +35,8 @@ sealed class PopUp(private val text: String?,
         val entityLocation = entity.location
         val armorStand = entity.world.spawn(
                 entityLocation.noised(noise).add(meanX, meanY, meanZ),
-                ArmorStand::class.java
-        ) { it.modelAsPop(text, popping) }
+                ArmorStand::class.java)
+        armorStand.buildAsPop(text)
 
         fun followEntity(ticks: Long) {
             if (!armorStand.isValid || ticks > lifeTime) {
@@ -48,7 +52,7 @@ sealed class PopUp(private val text: String?,
         return armorStand
     }
 
-    private fun ArmorStand.modelAsPop(text: String?, popping: (ArmorStand) -> Unit) {
+    private fun ArmorStand.buildAsPop(text: String?) {
         isVisible = false
         setBasePlate(false)
         setArms(true)
@@ -59,32 +63,34 @@ sealed class PopUp(private val text: String?,
         isSmall = true
         customName = text ?: ""
         isCustomNameVisible = text != null
-
-        popping(this)
     }
 
 }
 
 class StillPopUp(text: String? = null,
-                 popping: (ArmorStand) -> Unit = {},
-                 override val lifeTime: Long) : PopUp(text, popping)
+                 override val lifeTime: Long) : PopUp(text, { setGravity(false) })
 
-class SimplePopUp(text: String? = null,
-                  popping: (ArmorStand) -> Unit = {}) : PopUp(text, popping) {
+class SimplePopUp(text: String? = null) : PopUp(text, {
+    velocity = Vector(
+            Random.nextGaussian(variance = 0.03),
+            0.24,
+            Random.nextGaussian(variance = 0.03)
+    )
+}) {
 
     override val lifeTime = 5L
 
 }
 
-class LongPopUp(text: String? = null,
-                popping: (ArmorStand) -> Unit = {}) : PopUp(text, popping) {
+class LongPopUp(text: String? = null) : PopUp(text, {
+    velocity = Vector(
+            Random.nextGaussian(variance = 0.03),
+            0.24,
+            Random.nextGaussian(variance = 0.03)
+    )
+    Gigantic.PLUGIN.apply { server.scheduler.runTaskLater(this@apply, { _ -> setGravity(false) }, 5L) }
+}) {
 
     override val lifeTime = 15L
-
-    override fun pop(location: Location, noise: NoiseData): ArmorStand {
-        return super.pop(location, noise).also { armorStand ->
-            Gigantic.PLUGIN.apply { server.scheduler.runTaskLater(this@apply, { _ -> armorStand.setGravity(false) }, 5L) }
-        }
-    }
 
 }
