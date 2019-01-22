9b2d65661af977610254f72d139e227ab2b4c694
unicroak <unicroak@gmail.com>
2019-01-23 00:03:49 +0900

    withdraw entity of ArmorStand

--- src/main/kotlin/click/seichi/gigantic/popup/PopUp.kt
+++ src/main/kotlin/click/seichi/gigantic/popup/PopUp.kt
@@ -1,96 +1,39 @@
 package click.seichi.gigantic.popup
 
 import click.seichi.gigantic.Gigantic
-import click.seichi.gigantic.extension.noised
-import click.seichi.gigantic.util.NoiseData
-import click.seichi.gigantic.util.Random
+import click.seichi.gigantic.util.VirtualTag
 import org.bukkit.Location
-import org.bukkit.entity.ArmorStand
-import org.bukkit.entity.Entity
-import org.bukkit.util.Vector
 
 /**
  * @author tar0ss
  * @author unicroak
  */
-sealed class PopUp(private val text: String?,
-                   private val customizeOnPop: ArmorStand.() -> Unit = {}) {
-    // HACK: [customizeOnPop] should not exist
+class PopUp(private val animation: PopAnimation,
+            private val location: Location,
+            text: String) {
 
-    abstract val lifeTime: Long
-
-    fun pop(location: Location, noise: NoiseData = NoiseData()): ArmorStand {
-        return location.world.spawn(location.noised(noise), ArmorStand::class.java) {
-            it.buildAsPop(text)
-            Gigantic.PLUGIN.apply { server.scheduler.runTaskLater(this@apply, { _ -> if (it.isValid) it.remove() }, lifeTime) }
-            customizeOnPop(it)
-        }
+    companion object {
+        private const val SEND_DISTANCE = 32.0
     }
 
-    fun follow(entity: Entity,
-               meanX: Double = 0.0,
-               meanY: Double = 0.0,
-               meanZ: Double = 0.0,
-               noise: NoiseData = NoiseData()): ArmorStand {
-        val entityLocation = entity.location
-        val armorStand = entity.world.spawn(
-                entityLocation.noised(noise).add(meanX, meanY, meanZ),
-                ArmorStand::class.java)
-        armorStand.buildAsPop(text)
+    private val virtualTag = VirtualTag(location, text)
 
-        fun followEntity(ticks: Long) {
-            if (!armorStand.isValid || ticks > lifeTime) {
-                armorStand.remove()
-            } else {
-                armorStand.teleport(entityLocation.noised(noise).add(meanX, meanY, meanZ))
-                Gigantic.PLUGIN.apply { server.scheduler.runTaskLater(this@apply, { _ -> followEntity(ticks + 1) }, 1L) }
-            }
-        }
+    fun pop() {
+        location.world.players
+                .filter { player -> player.location.distanceSquared(location) > SEND_DISTANCE }
+                .forEach { player ->
+                    animation.animate(player, virtualTag, location)
 
-        followEntity(0L)
-
-        return armorStand
+                    Gigantic.PLUGIN.apply {
+                        server.scheduler.runTaskLater(
+                                this@apply,
+                                { _ ->
+                                    if (player.isValid) virtualTag.sendDestroyPacket(player)
+                                },
+                                animation.lifetime
+                        )
+                    }
+                }
     }
-
-    private fun ArmorStand.buildAsPop(text: String?) {
-        isVisible = false
-        setBasePlate(false)
-        setArms(true)
-        isMarker = true
-        isInvulnerable = true
-        canPickupItems = false
-        setGravity(true)
-        isSmall = true
-        customName = text ?: ""
-        isCustomNameVisible = text != null
-    }
-
-}
-
-class StillPopUp(text: String? = null,
-                 override val lifeTime: Long) : PopUp(text, { setGravity(false) })
-
-class SimplePopUp(text: String? = null) : PopUp(text, {
-    velocity = Vector(
-            Random.nextGaussian(variance = 0.03),
-            0.24,
-            Random.nextGaussian(variance = 0.03)
-    )
-}) {
-
-    override val lifeTime = 5L
-
-}
-
-class LongPopUp(text: String? = null) : PopUp(text, {
-    velocity = Vector(
-            Random.nextGaussian(variance = 0.03),
-            0.24,
-            Random.nextGaussian(variance = 0.03)
-    )
-    Gigantic.PLUGIN.apply { server.scheduler.runTaskLater(this@apply, { _ -> setGravity(false) }, 5L) }
-}) {
-
-    override val lifeTime = 15L
 
 }
