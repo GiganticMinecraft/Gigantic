9f4d54a01b809cb017b894e20ecc238666122afc
unicroak <unicroak@gmail.com>
2019-01-26 13:09:45 +0900

    apply on 1.13.2

--- src/main/kotlin/click/seichi/gigantic/util/VirtualTag.kt
+++ src/main/kotlin/click/seichi/gigantic/util/VirtualTag.kt
@@ -2,6 +2,7 @@ package click.seichi.gigantic.util
 
 import click.seichi.gigantic.Gigantic
 import com.comphenix.protocol.PacketType
+import com.comphenix.protocol.wrappers.WrappedChatComponent
 import com.comphenix.protocol.wrappers.WrappedDataWatcher
 import org.bukkit.Location
 import org.bukkit.entity.Player
@@ -14,16 +15,14 @@ import java.util.*
 class VirtualTag(var location: Location,
                  private val text: String,
                  private val id: Int = Random.nextInt(Int.MAX_VALUE),
-                 private val uuid: UUID = UUID.randomUUID(),
-                 private var noGravity: Boolean = true) {
+                 private val uuid: UUID = UUID.randomUUID()) {
 
     companion object {
         private const val BIT_IS_SMALL = 0x01
         private const val BIT_NO_BASE_PLATE = 0x08
         private const val BIT_SET_MARKER = 0x10
         private const val BIT_INVISIBLE = 0x20
-        private const val ARMOR_STAND_ID = 30
-        private const val ARMOR_STAND_HEIGHT = 2.25
+        private const val ARMOR_STAND_ID = 1
 
         private val protocolManager = Gigantic.PROTOCOL_MG
     }
@@ -38,7 +37,7 @@ class VirtualTag(var location: Location,
 
         packet.doubles.apply {
             write(0, location.x)
-            write(1, location.y - ARMOR_STAND_HEIGHT)
+            write(1, location.y)
             write(2, location.z)
         }
 
@@ -46,13 +45,17 @@ class VirtualTag(var location: Location,
             write(0, uuid)
         }
 
-        packet.dataWatcherModifier.values[0].apply {
-            setObject(createDataWatcherObject<Byte>(0), BIT_INVISIBLE)
-            setObject(createDataWatcherObject<Byte>(2), BIT_IS_SMALL + BIT_NO_BASE_PLATE + BIT_SET_MARKER)
-            setObject(createDataWatcherObject<String>(2), text)
-            setObject(createDataWatcherObject<Boolean>(1), true)      // SILENT
-            setObject(createDataWatcherObject<Boolean>(2), noGravity) // NO_GRAVITY
-        }
+        packet.dataWatcherModifier.writeDefaults()
+        packet.dataWatcherModifier.write(
+                0,
+                WrappedDataWatcher().apply {
+                    setObject(createDataWatcherObject(2, WrappedDataWatcher.Registry.getChatComponentSerializer(true)), Optional.of(WrappedChatComponent.fromText(text).handle))
+                    setObject(createDataWatcherObject<Byte>(0), (BIT_INVISIBLE).toByte())
+                    setObject(createDataWatcherObject<Byte>(11), (BIT_IS_SMALL + BIT_NO_BASE_PLATE + BIT_SET_MARKER).toByte())
+                    setObject(createDataWatcherObject<Boolean>(3), true) // NAME_VISIBLE
+                    setObject(createDataWatcherObject<Boolean>(4), true) // SILENT
+                }
+        )
 
         protocolManager.sendServerPacket(player, packet)
     }
@@ -62,28 +65,9 @@ class VirtualTag(var location: Location,
 
         packet.integers.apply {
             write(0, id)
-        }
-
-        packet.shorts.apply {
-            write(0, (delta.x * 4096).toShort())
-            write(1, (delta.y * 4096).toShort())
-            write(2, (delta.z * 4096).toShort())
-        }
-
-        protocolManager.sendServerPacket(player, packet)
-    }
-
-    fun sendTeleportPacket(player: Player) {
-        val packet = protocolManager.createPacket(PacketType.Play.Server.ENTITY_TELEPORT)
-
-        packet.integers.apply {
-            write(0, id)
-        }
-
-        packet.doubles.apply {
-            write(0, location.x)
-            write(1, location.y - ARMOR_STAND_HEIGHT)
-            write(2, location.z)
+            write(1, (delta.x * 4096).toInt())
+            write(2, (delta.y * 4096).toInt())
+            write(3, (delta.z * 4096).toInt())
         }
 
         protocolManager.sendServerPacket(player, packet)
@@ -92,24 +76,8 @@ class VirtualTag(var location: Location,
     fun sendDestroyPacket(player: Player) {
         val packet = protocolManager.createPacket(PacketType.Play.Server.ENTITY_DESTROY)
 
-        packet.integers.apply {
-            write(0, 1)
-        }
-
         packet.integerArrays.apply {
-            write(0, arrayOf(1).toIntArray())
-        }
-
-        protocolManager.sendServerPacket(player, packet)
-    }
-
-    fun sendChangeGravity(player: Player, noGravity: Boolean) {
-        val packet = protocolManager.createPacket(PacketType.Play.Server.ENTITY_STATUS)
-
-        this.noGravity = noGravity
-
-        packet.dataWatcherModifier.values[0].apply {
-            setObject(createDataWatcherObject<Boolean>(2), noGravity)
+            write(0, arrayOf(id).toIntArray())
         }
 
         protocolManager.sendServerPacket(player, packet)
@@ -117,5 +85,9 @@ class VirtualTag(var location: Location,
 
     private inline fun <reified T : Any> createDataWatcherObject(index: Int) =
             WrappedDataWatcher.WrappedDataWatcherObject(index, WrappedDataWatcher.Registry.get(T::class.java))
+
+
+    private fun createDataWatcherObject(index: Int, serializer: WrappedDataWatcher.Serializer): WrappedDataWatcher.WrappedDataWatcherObject =
+            WrappedDataWatcher.WrappedDataWatcherObject(index, serializer)
 
 }
