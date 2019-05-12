2fff29f7f2b109ff64b4d5d9e67aaa33ff092584
tar0ss <syas1215@icloud.com>
2019-05-12 11:38:42 +0900

    Remove unused

--- src/main/kotlin/click/seichi/gigantic/sidebar/bars/EthelLogger.kt
+++ src/main/kotlin/click/seichi/gigantic/sidebar/bars/EthelLogger.kt
@@ -59,45 +59,4 @@ object EthelLogger : Logger("ethel") {
                 ")"
         )
     }
-
-/*
-    override fun getMessageMap(player: Player): Map<SideBarRow, String> {
-        val willMap = Will.values()
-                .filter { player.hasAptitude(it) }
-                .map { it to player.ethel(it) }.toMap()
-        return willMap.keys.filter {
-            willMap.getValue(it) > 0
-        }.filter {
-            // TODO ここはこの実装だと新意志実装時に追記し忘れる可能性あり
-            // イベントは特殊な形でフィルター
-            when (it) {
-                Will.SAKURA -> GiganticEvent.SAKURA.isActive()
-                Will.MIO -> GiganticEvent.MIO.isActive()
-                else -> true
-            }
-        }.map { will ->
-            val row = when (will) {
-                Will.AQUA -> SideBarRow.TWO
-                Will.IGNIS -> SideBarRow.THREE
-                Will.AER -> SideBarRow.FIVE
-                Will.TERRA -> SideBarRow.ONE
-                Will.NATURA -> SideBarRow.FOUR
-                Will.GLACIES -> SideBarRow.SIX
-                Will.LUX -> SideBarRow.NINE
-                Will.SOLUM -> SideBarRow.SEVEN
-                Will.UMBRA -> SideBarRow.TEN
-                Will.VENTUS -> SideBarRow.EIGHT
-                else -> SideBarRow.FOURTEEN
-            }
-            row to "${will.chatColor}${ChatColor.BOLD}" +
-                    "${will.getName(player.wrappedLocale).let {
-                        if (it.length == 2) it
-                        else " $it "
-                    }}:" +
-                    "${ChatColor.RESET}" +
-                    "${will.chatColor}" +
-                    "${willMap.getValue(will).coerceAtMost(999)}".padStart(4, ' ')
-
-        }.toMap()
-    }*/
 }
