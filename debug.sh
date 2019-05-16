ce97fb5c0571978a64d77c39ee0893d59de80bb4
tar0ss <syas1215@icloud.com>
2019-05-17 02:07:01 +0900

    Update to 1.14.1

--- debug.sh
+++ debug.sh
@@ -8,7 +8,7 @@ sh copy.sh
 
 # サーバー起動
 cd ./server
-java -Dfile.encording=UTF-8 -verbose:gc -server -Xms2G -Xmx2G -XX:MetaspaceSize=512M -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+DisableExplicitGC -XX:+UseCompressedOops -XX:+OptimizeStringConcat -XX:+UseTLAB -jar spigot-1.14.jar
+java -Dfile.encording=UTF-8 -verbose:gc -server -Xms2G -Xmx2G -XX:MetaspaceSize=512M -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+DisableExplicitGC -XX:+UseCompressedOops -XX:+OptimizeStringConcat -XX:+UseTLAB -jar spigot-1.14.1.jar
 cd ../
 
 echo "Restarting in 3 seconds,please wait..."
